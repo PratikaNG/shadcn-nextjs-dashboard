0d955e5a3304722c49f4c91d454d9c037241f484
Pratika NG <pratika.05ng@gmail.com>
2025-10-22 13:45:59 +0530

    theme settings

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,8 @@
-import { LogOut, Moon, Settings, User } from 'lucide-react'
+"use client"
+import { LogOut, Moon, Settings, User,Sun } from 'lucide-react'
 import Link from 'next/link'
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { useTheme } from "next-themes"
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -9,8 +11,10 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { Button } from './ui/button'
 
 function Navbar() {
+    const { setTheme } = useTheme()
   return (
     <nav className="p-4 flex items-center justify-between">
       {/* left */}
@@ -18,7 +22,26 @@ function Navbar() {
       {/* right */}
       <div className="flex items-center gap-4">
         <Link href="/">Dashboard</Link>
-        <Moon />
+       <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
         <DropdownMenu>
           <DropdownMenuTrigger>
         <Avatar>
